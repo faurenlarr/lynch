d5987067b4d8c132e363a0ca0d5431f52a050ad0
Brandon Jones <bdjones2410@gmail.com>
2015-11-03 15:15:33 -0500

    added dummy text and starter comments to keep sections organized across members

---
+++ templates.js
@@ -0,0 +1,11 @@
+// this section is used to define templates inside this array.
+
+var templates =[
+  templateExample:[
+    'a cool example of stuff',
+    'containing html like <h3></h3>',
+    'and if you want your variables',
+    'do this<h1><%=name%></h1>',
+  ].join(""),
+
+]
