f2fdb455734c5924a388724391b5cc05fa10c0a9
Brandon Jones <bdjones2410@gmail.com>
2015-11-04 15:26:14 -0500

    fixed enter button issues on html and main.js click function on submit buttons

--- main.js
+++ main.js
@@ -1,165 +1,165 @@
 $(document).ready(function() {
-    main.init();
+   main.init();
 });
 
 
 var main ={
-     urlMessages: "https://tiny-tiny.herokuapp.com/collections/lynchberg/",
-    //  urlUsers:"https://tiny-tiny.herokuapp.com/collections/lynchUsers/",
+    urlMessages: "https://tiny-tiny.herokuapp.com/collections/lynchberg/",
+    urlUsers:"https://tiny-tiny.herokuapp.com/collections/lynchUser/",
 
 
-    init:function() {
-      main.styling();
-      main.events();
-    },
-
-    styling: function(){
-      main.grabMessages();
-
-    },
-
-    events:function(){
-
-
-    $('.textbox').on('click', '.subbut',function(e) {
-        e.preventDefault();
-        var messageText = $(this).siblings('input[name="message"]').val();
-        var data ={
-          username: "bdjones",
-          message: messageText,
-          avatar: "http://31.media.tumblr.com/fffd0f8677c5f75e47bfbaa9a17c44e9/tumblr_neyjbn8JGm1texwuzo1_400.gif",
-        };
-        main.postMessage(data);
-        $(this).siblings('input[name="message"]').val(' ');
-      });
-      $('.textbox').keypress('.message',function(e){
-       if(e.which == 13){//Enter key pressed
-           $('.message').click();//Trigger search button click event
-       }
-     });
-     $('.chatbox').on('click','.delete-button',function(){
-       var id = $(this).parent('div').attr('id');
-       main.deleteMessages(id);
-     });
-
-
-      $('section').on('click', '.signInSubmit', function(e) {
-        e.preventDefault();
-        $('.page1').addClass('hidden');
-        $('.container').removeClass('hidden');
-        var userEntry = $(this).siblings('input[name="username"]').val();
-        var avatarEntry = $(this).siblings('input[name="avatar"]').val();
-      });
+   init:function() {
+     main.styling();
+     main.events();
    },
 
+   styling: function(){
+     main.grabMessages();
+
+   },
+
+   events:function(){
+
+   $('.textbox').on('click', '.subbut',function(e) {
+
+       e.preventDefault();
+       var messageText = $(this).siblings('input[name="message"]').val();
+       var data ={
+         username: "cglane",
+         message: messageText,
+         avatar: "http://31.media.tumblr.com/fffd0f8677c5f75e47bfbaa9a17c44e9/tumblr_neyjbn8JGm1texwuzo1_400.gif (27KB)",
+       };
+       main.postMessage(data);
+
+       $(this).siblings('input[name="message"]').val(' ');
+     });
+     $('.textbox').keypress(function(e){
+      if(e.which == 13){//Enter key pressed
+          $('.subbut').click();//Trigger search button click event
+      }
+    });
+    $('.chatbox').on('click','.delete-button',function(){
+      var id = $(this).parent('div').attr('id');
+      main.deleteMessages(id);
+    });
 
 
-  loadMessages:function(data){
-    var html = " ";
-    var tmpl = _.template(templates.userInput);
-    console.log(tmpl);
-    _.each(data, function(el){
-      console.log(el);
-      html += tmpl(el);
-  });
-    $('.chatfield').html(html);
+     $('section').on('click', '.signInSubmit', function(e) {
+       e.preventDefault();
+       $('.page1').addClass('hidden');
+       $('.container').removeClass('hidden');
+       var userEntry = $(this).siblings('input[name="username"]').val();
+       var avatarEntry = $(this).siblings('input[name="avatar"]').val();
+     });
   },
 
-  loadUsers:function(data){
 
-    var tmpl = _.templates(templates.messageTemplate);
-    $('aside .users').append(tmpl(data));
-    console.log(data);
-  },
+
+
+ loadMessages:function(data){
+   var html = " ";
+   var tmpl = _.template(templates.userInput);
+   _.each(data, function(el){
+     html += tmpl(el);
+ });
+   $('.chatfield').html(html);
+
+ },
+
+ loadUsers:function(data){
+   var tmpl = _.templates(templates.activeUsers);
+   $('aside .users').append(tmpl(data));
+   console.log(data);
+ },
 
 
 grabUsers:function(){
-    $.ajax({
-      url:main.urlUsers,
-      method:'GET',
-      success:function(users){
-        console.log(users);
-        main.loadUsers(users);
-      }
-    });
+   $.ajax({
+     url:main.urlUsers,
+     method:'GET',
+     success:function(users){
+       console.log(users);
+       main.loadUsers(users);
+     }
+   });
 },
-  postUsers:function(user){
-    $.ajax({
-      url:main.urlUsers,
-      method:'POST',
-      data: user,
-      success:function(data){
-        console.log(data);
-      },
-      failure:function(data){
-        console.log("You are a failure" + data);
-      }
-    });
-  },
-  deleteUsers:function(userId){
-      $.ajax({
-        url: main.urlUsers + userId,
-        method: 'DELETE',
-        success:function(data){
-          console.log(data + "deleted");
+ postUsers:function(user){
+   $.ajax({
+     url:main.urlUsers,
+     method:'POST',
+     data: user,
+     success:function(data){
+       console.log(data);
+     },
+     failure:function(data){
+       console.log("You are a failure" + data);
+     }
+   });
+ },
+ deleteUsers:function(userId){
+     $.ajax({
+       url: main.urlUsers + userId,
+       method: 'DELETE',
+       success:function(data){
+         console.log(data + "deleted");
 
-        },
-        failure:function(){
-          console.log(data+ " :not deleted, idiot");
-        }
+       },
+       failure:function(){
+         console.log(data+ " :not deleted, idiot");
+       }
 
-      });
-  },
+     });
+ },
 
-  postMessage: function(message) {
-  $.ajax({
-    url: main.urlUsers,
-    method: 'POST',
-    data: message,
-    success: function(resp) {
-      console.log(resp);
-      var tmpl = _.template(templates.userInput);
-      $('.chatfield').append(tmpl(resp));
-    },
-    failure: function(resp) {
-      console.log("FAILURE", resp);
-    }
-  });
+ postMessage: function(message) {
+ $.ajax({
+   url: main.urlMessages,
+   method: 'POST',
+   data: message,
+   success: function(resp) {
+     console.log(resp);
+     var tmpl = _.template(templates.userInput);
+     $('.chatfield').append(tmpl(resp));
+   },
+   failure: function(resp) {
+     console.log("FAILURE", resp);
+   }
+ });
 },
 grabMessages: function() {
-  $.ajax({
-    type: 'GET',
-    url: main.urlMessages,
-    success: function(data) {
-      main.loadMessages(data);
-    },
-    failure: function(data) {
-      console.log("FAILURE: ", data);
-    }
-  });
+ $.ajax({
+   type: 'GET',
+   url: main.urlMessages,
+   success: function(data) {
+     main.loadMessages(data);
+   },
+   failure: function(data) {
+     console.log("FAILURE: ", data);
+   }
+ });
 },
 
 deleteMessages: function(messageId) {
-  $.ajax({
-    method: 'DELETE',
-    url: main.urlMessages + messageId,
-    success: function(data) {
-      console.log("DELETED", data);
-      var id = '#' + messageId;
-      $(id).remove();
-    },
-    failure: function(data) {
-      console.log("ERROR", data);
-    }
-  });
+ $.ajax({
+   method: 'DELETE',
+   url: main.urlMessages + messageId,
+   success: function(data) {
+     console.log("DELETED", data);
+     var id = '#' + messageId;
+     $(id).remove();
+   },
+   failure: function(data) {
+     console.log("ERROR", data);
+   }
+ });
 },
 deleteAll:function(){
-  $.ajax({
-    method: 'DELETE',
-    url:main.urlMessages,
-    success:function(){
-      console.log('all deleted');
-    }
-  })
+ $.ajax({
+   method: 'DELETE',
+   url:main.urlMessages,
+   success:function(){
+     console.log('all deleted');
+   }
+ })
 }
 };
